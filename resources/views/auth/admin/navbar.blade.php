53aba039ebc9ba851340b8e2bfdd6544c397cf87
hxpdong <dongstt1096@gmail.com>
2023-10-07 07:43:41 +0700

    fix header of admin

--- resources/views/auth/admin/navbar.blade.php
+++ resources/views/auth/admin/navbar.blade.php
@@ -53,9 +53,10 @@
                             </div>
                         </nav>
                     </div>
-                    <a href="/logoutHeader"
-                                class="text-gray-800 bg-white mx-1 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 md:px-5 py-2 md:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Đăng
-                                xuất</a>
+                    <button onclick="window.location.href = '/logoutHeader'; localStorage.clear();"
+                        class="text-gray-800 bg-white mx-1 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Đăng
+                        xuất
+                    </button>
                     @endif
                 </div>
             </div>
@@ -107,9 +108,10 @@
                                     </div>
                                 </nav>
                             </div>
-                            <a href="/logoutHeader"
-                                class="text-gray-800 bg-white mx-1 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 md:px-5 py-2 md:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Đăng
-                                xuất</a>
+                            <button onclick="window.location.href = '/logoutHeader'; localStorage.clear();"
+                                class="text-gray-800 bg-white mx-1 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Đăng
+                                xuất
+                            </button>
                             @endif
                         </div>
                     </li>
